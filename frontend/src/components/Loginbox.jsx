c6c431eb244807745e236dcf7e408a23806c475f
AnamayNarkar <anamay.narkar.102@gmail.com>
2024-06-08 00:17:31 +0530

    setup state handling for login and signup forms

--- frontend/src/components/Loginbox.jsx
+++ frontend/src/components/Loginbox.jsx
@@ -3,7 +3,6 @@ import "../styles/Loginbox.css";
 
 function Loginbox() {
   const [showSignUp, setShowSignUp] = useState(false);
-
   const redDivStyles = {
     left: showSignUp ? "50%" : "0",
     right: showSignUp ? "0" : "50%",
@@ -12,6 +11,36 @@ function Loginbox() {
 
   const handleToggleForm = () => {
     setShowSignUp((prevState) => !prevState);
+  };
+
+  const [loginData, setLoginData] = useState({
+    usernameoremail: "",
+    password: "",
+  });
+
+  const [signUpData, setSignUpData] = useState({
+    name: "",
+    email: "",
+    username: "",
+    password: "",
+  });
+
+  const handleLoginChange = (event) => {
+    setLoginData({ ...loginData, [event.target.name]: event.target.value });
+    console.log(loginData);
+  };
+
+  const handleSignUpChange = (event) => {
+    setSignUpData({ ...signUpData, [event.target.name]: event.target.value });
+    console.log(signUpData);
+  };
+
+  const handleSignUpSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleLoginSubmit = (event) => {
+    event.preventDefault();
   };
 
   return (
@@ -23,17 +52,65 @@ function Loginbox() {
           {showSignUp ? "Already have an account?" : "Don't have an account?"}
         </p>
       </div>
-      <form className={`signUpForm ${showSignUp ? "" : "hidden"}`}>
-        <input className="setNameInput" placeholder="Enter your Name"/>
-        <input className="setEmailInput" placeholder="Enter your Email"/>
-        <input className="setUsernameInput" placeholder="Set your Username"/>
-        <input  className="setPasswordInput" placeholder="Set your Password"/>
-        <button>Sign Up</button>
+      <form
+        className={`signUpForm ${showSignUp ? "" : "hidden"}`}
+        onSubmit={handleSignUpSubmit}
+      >
+        <input
+          className="setNameInput"
+          placeholder="Enter your Name"
+          name="name"
+          value={signUpData.name}
+          onChange={handleSignUpChange}
+          autoComplete="off"
+        />
+        <input
+          className="setEmailInput"
+          placeholder="Enter your Email"
+          name="email"
+          value={signUpData.email}
+          onChange={handleSignUpChange}
+          autoComplete="off"
+        />
+        <input
+          className="setUsernameInput"
+          placeholder="Set your Username"
+          name="username"
+          value={signUpData.username}
+          onChange={handleSignUpChange}
+          autoComplete="off"
+        />
+        <input
+          className="setPasswordInput"
+          placeholder="Set your Password"
+          name="password"
+          value={signUpData.password}
+          onChange={handleSignUpChange}
+          autoComplete="off"
+        />
+        <button type="submit">Sign Up</button>
       </form>
-      <form className={`loginForm ${showSignUp ? "hidden" : ""}`}>
-        <input className="enterUsernameOrEmailInput" placeholder="Enter Username or Email"/>
-        <input className="enterPasswordInput" placeholder="Enter your password"/>
-        <button>Login</button>
+      <form
+        className={`loginForm ${showSignUp ? "hidden" : ""}`}
+        onSubmit={handleLoginSubmit}
+      >
+        <input
+          className="enterUsernameOrEmailInput"
+          placeholder="Enter Username or Email"
+          name="usernameoremail"
+          value={loginData.usernameoremail}
+          onChange={handleLoginChange}
+          autoComplete="off"
+        />
+        <input
+          className="enterPasswordInput"
+          placeholder="Enter your password"
+          name="password"
+          value={loginData.password}
+          onChange={handleLoginChange}
+          autoComplete="off"
+        />
+        <button type="submit">Login</button>
       </form>
     </div>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import App from './components/App.jsx'
 import Login from './components/Login.jsx'
 
+const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <App /> : <Login setIsLoggedIn={setIsLoggedIn}/>} />
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter >
 )
